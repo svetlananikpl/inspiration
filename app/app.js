78a70a8759f350e2b5ba70e0b3dccc39dec66b77
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2015-12-07 14:53:58 +0300

    add ui grid

--- app/app.js
+++ app/app.js
@@ -5,8 +5,34 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'ui.grid'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+  $scope.myData = [
+    {
+      "firstName": "Cox",
+      "lastName": "Carney",
+      "company": "Enormo",
+      "employed": true
+    },
+    {
+      "firstName": "Lorraine",
+      "lastName": "Wise",
+      "company": "Comveyer",
+      "employed": false
+    },
+    {
+      "firstName": "Nancy",
+      "lastName": "Waters",
+      "company": "Fuelton",
+      "employed": false
+    }
+  ];
 }]);
