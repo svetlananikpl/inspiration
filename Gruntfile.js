7e68d211e4982ed95bca7f52da7519b7409b8ba8
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-01-12 17:42:05 +0300

    add grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,56 @@
 /**
  * Created by Admin on 11.01.2016.
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    require('time-grunt')(grunt);
+    grunt.initConfig({
 
-    require('load-grunt-config')(grunt, {
-        jitGrunt: true
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
+                    'app/**/*.js',
+                    'app.js'
+                ],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'dist/inspiration.js',
+                dest: 'dist/inspiration.min.js'
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['app/**/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: 'dist'
+                }
+            }
+        }
+
     });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect', 'concat', 'uglify']);
+
 };
